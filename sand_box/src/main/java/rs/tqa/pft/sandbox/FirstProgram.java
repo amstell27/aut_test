5ce2c5d417323c0c76394e158a4ce2f65b703ee2
Amstell <sergei_i27@mail.ru>
2018-03-22 18:45:11 +0700

    Добавлены переменные

--- sand_box/src/main/java/rs/tqa/pft/sandbox/FirstProgram.java
+++ sand_box/src/main/java/rs/tqa/pft/sandbox/FirstProgram.java
@@ -3,7 +3,13 @@ package rs.tqa.pft.sandbox;
 public class FirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String somebody = "world";
+        System.out.println("Hello, " + somebody + "!");
+
+        double l = 9;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+
     }
 
 }

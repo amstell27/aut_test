876cea5472b61a4c9bc4507b1743c6b55df654d0
Amstell <sergei_i27@mail.ru>
2018-03-30 11:16:25 +0700

    Добавлены функции по вычислению фигур

--- sand_box/src/main/java/rs/tqa/pft/sandbox/FirstProgram.java
+++ sand_box/src/main/java/rs/tqa/pft/sandbox/FirstProgram.java
@@ -3,13 +3,27 @@ package rs.tqa.pft.sandbox;
 public class FirstProgram {
 
     public static void main(String[] args) {
-        String somebody = "world";
-        System.out.println("Hello, " + somebody + "!");
 
-        double l = 9;
-        double s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+        hello("world");
+        hello("user");
 
+        double i = 5;
+        double j = 6;
+        System.out.println("Площадь квадрата со стороной " + i + " = " + area(i));
+        System.out.println("Площадь прямоугольника со сторонами " + i + " и " + j+ " = " + area(i,j));
+    }
+
+    public static void hello (String somebody) {
+
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area (double l) {
+        return l*l;
+    }
+
+    public static double area(double a, double b){
+        return a*b;
     }
 
 }
